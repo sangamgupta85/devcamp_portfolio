ea0646d454e8da23ecb4d783745417cf9408aee9
Sangam Gupta <sangam.gupta@mind-infotech.com>
2020-12-17 11:49:14 +0530

    Added sample data via seeds file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_054629) do
+ActiveRecord::Schema.define(version: 2020_12_16_063011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_12_15_054629) do
   create_table "portfolios", force: :cascade do |t|
     t.string "title"
     t.string "subtitle"
+    t.text "body"
     t.text "main_image"
     t.text "thumb_image"
     t.datetime "created_at", null: false
