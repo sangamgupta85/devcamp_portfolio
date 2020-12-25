8a4c6993146523bb11f3e439f7bc7dbc98c4c565
Sangam Gupta <sangam.gupta@mind-infotech.com>
2020-12-25 12:47:52 +0530

    Added create and edit buttons

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -12,9 +12,25 @@ class PortfoliosController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_url, notice: 'portfolio item was successfully created.' }
+        format.html { redirect_to portfolios_path, notice: 'portfolio item was successfully created.' }
       else
         format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_url, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
       end
     end
   end
