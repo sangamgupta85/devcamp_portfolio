76d2cdcebad3c90bd6ee655491036ca1d891faac
Sangam Gupta <sangam.gupta@mind-infotech.com>
2020-12-30 17:36:11 +0530

    Implemented nested form portfolio technologies

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -10,6 +10,7 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.new }
   end
 
   def create
@@ -56,6 +57,6 @@ class PortfoliosController < ApplicationController
   private
   # Only allow a list of trusted parameters through.
   def portfolio_params
-    params.require(:portfolio).permit(:title, :subtitle, :body)
+    params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
   end
 end
