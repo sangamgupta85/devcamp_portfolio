5739faa3862853817bab922242fe164c79486356
Sangam Gupta <sangam.gupta@mind-infotech.com>
2021-01-04 11:44:27 +0530

    Alignment changes

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,5 @@ module DevcampPortfolio
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-
-    config.action_controller.permit_all_parameters = true
   end
 end
